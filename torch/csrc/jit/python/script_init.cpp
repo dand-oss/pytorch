90a1a24d459d5608c1dae7042d2f4d9e57ec365b
Dan Dees <dand@appsmiths.com>
2021-05-13 08:27:23 +0700

    fix gcc

--- torch/csrc/jit/python/script_init.cpp
+++ torch/csrc/jit/python/script_init.cpp
@@ -1218,7 +1218,7 @@ void initJitScriptBindings(PyObject* module) {
              const std::string& method_name,
              const py::tuple& input_tuple) {
             Stack stack;
-            for (auto& input : input_tuple) {
+            for (auto&& input : input_tuple) {
               stack.push_back(toTypeInferredIValue(input));
             }
             return m.get_method(method_name)(stack);
@@ -1229,7 +1229,7 @@ void initJitScriptBindings(PyObject* module) {
           "forward",
           [](mobile::Module& m, const py::tuple& input_tuple) {
             Stack stack;
-            for (auto& input : input_tuple) {
+            for (auto&& input : input_tuple) {
               stack.push_back(toTypeInferredIValue(input));
             }
             return m.get_method("forward")(stack);
